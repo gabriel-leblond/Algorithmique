0404928b2c0ead2ed508e7ceb3b7d9a64992c388
Gabriel Leblond <120410801+gabriel-leblond@users.noreply.github.com>
2023-11-12 17:57:03 +0100

    INIT - somme de N

---
+++ somme_de_n.js
@@ -0,0 +1,12 @@
+// Écrire une fonction récursive qui permet de
+// calculer la somme 1+2+3+...+ N. où N saisi
+// par l'utilisateur. (fonction récursive).
+
+//console.log('start');
+
+function sommeDeN(params) {
+  console.log(`params d'entrée : ${params}`);
+
+}
+
+sommeDeN(6);
