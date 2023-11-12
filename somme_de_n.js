f9f8cb9feacd4f2727c1a066f65938a65980a5d5
Gabriel Leblond <120410801+gabriel-leblond@users.noreply.github.com>
2023-11-12 18:04:57 +0100

    PASS - somme de N

--- somme_de_n.js
+++ somme_de_n.js
@@ -1,12 +1,22 @@
 // Écrire une fonction récursive qui permet de
 // calculer la somme 1+2+3+...+ N. où N saisi
-// par l'utilisateur. (fonction récursive).
+// par l'utilisateur.
 
 //console.log('start');
 
 function sommeDeN(params) {
   console.log(`params d'entrée : ${params}`);
 
+  let tempValue = params - 1;
+
+  while (tempValue >= 1) {
+    params = params + tempValue;
+    console.log(tempValue);
+    tempValue --;
+  }
+
+  console.log(`params de sortie : ${params}`);
+  return params
 }
 
 sommeDeN(6);
